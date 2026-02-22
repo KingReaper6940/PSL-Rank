f06fc6f632d8e81724b463ba6ee1d7bb719a5a9d
Vrishn Viswa Sathyamoorthy <vms5663@psu.edu>
2026-02-22 11:29:21 -0500

    chore: added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Vote from './pages/Vote'
 import Submit from './pages/Submit'
 import Profile from './pages/Profile'
 import Stats from './pages/Stats'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="/stats" element={<Stats />} />
       </Routes>
+      <Analytics />
     </>
   )
 }
